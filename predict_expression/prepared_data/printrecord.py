252c76b6d71bad2ebde34ce7a1035c3f38fed6aa
Vikram Agarwal <vagar@sci-pvm-vagar.calicolabs.local>
2020-10-20 13:33:06 -0700

    Testing the addition of Xpresso to Kipoi

--- predict_expression/prepared_data/printrecord.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-for example in tf.python_io.tf_record_iterator("expression_level_predict_pca-test-00000-of-00010"):
-    example = tf.train.Example.FromString(example)
-    print(example)
-    # this = tf.reshape(example['inputs'], [1, 1001, 117])
-    # print(example['inputs'])
-    exit()
